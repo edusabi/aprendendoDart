2610ea8a026be6525908800795d3850cf2713047
Edu <luis_100404@hotmail.com>
2025-04-15 14:41:19 -0300

    add new files

--- aula6/condicoesAndEor.dart
+++ aula6/condicoesAndEor.dart
@@ -1,3 +1,20 @@
 void main() {
+  String clima;
+  clima = "Chuva";
+  int temperatura = 20;
+  bool sair = true;
 
+  // && AND
+  if (clima == 'Sol' && temperatura > 20) {
+    print("Vou sair de casa");
+  } else {
+    print("Vou ficar em casa");
+  }
+
+  // || OR
+  if (clima == 'Sol' || temperatura > 20 || sair == true) {
+    print("Vou sair de casa");
+  } else {
+    print("Vou ficar em casa");
+  }
 }
---
+++ aula6/operadorTernario.dart
@@ -0,0 +1,16 @@
+void main() {
+  bool condicao = false;
+  String nome = "Luis";
+  String clima = "Chuva";
+
+  print(condicao ? "Condição verdadeira" : "Condiçao falsa");
+
+  print(2 < 3 ? "Menor" : "Maior ou igual");
+
+  print(nome == "Luis" ? "Nome ok!" : "Nome errado!");
+
+  String mensagem = clima == "Sol" ? "Agora vou sair!" : "Tomara que saia sol!";
+  
+  print(mensagem);
+
+}
