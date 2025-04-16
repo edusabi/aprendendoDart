333880a4edfb3d9b85318ff020a74450dcaba3ab
Edu <luis_100404@hotmail.com>
2025-04-16 15:40:59 -0300

    new files

---
+++ aula7/switchCase.dart
@@ -0,0 +1,19 @@
+void main() {
+  String tela = "";
+
+  switch (tela) {
+
+    case "Home":
+      print("Página inicial!");
+      break;
+
+    case "Splash screen":
+      print("Splash screen!");
+      break;
+
+    default:
+      print("Nenhuma tela!");
+      break;
+  }
+
+}
---
+++ aula8/for.dart
@@ -0,0 +1,11 @@
+void main() {
+  for (int a = 0; a <= 10; a++) {
+    print("$a x 2 =  ${a * 2}");
+  }
+
+  print(" ");
+
+  for (int i = 0; i <= 10; i++) {
+    print("$i x 5 =  ${i * 5}");
+  }
+}
